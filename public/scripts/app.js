70da1cca065fc181cedcd9bcc72f628727653054
viktmv <viktmv@icloud.com>
2017-05-24 21:52:38 +0000

    add .eslint config and linted app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,19 @@
 $(() => {
   $.ajax({
-    method: "GET",
-    url: "/api/users"
+    method: 'GET',
+    url: '/api/users'
   }).done(users => {
-    for (user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+    for (var user of users) {
+      $('<div>').text(user.name).appendTo($('body'));
     }
   })
 
   $.ajax({
-    method: "GET",
-    url: "/restaurants"
+    method: 'GET',
+    url: '/restaurants'
   }).done(restaurants => {
-    for(restaurant of restaurants) {
-      $("<div>").text(restaurant.name).appendTo($("body"));
+    for(var restaurant of restaurants) {
+      $('<div>').text(restaurant.name).appendTo($('body'));
     }
   })
 })
