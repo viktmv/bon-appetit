478e1e78b6ca549c5685a0f2c9d82ad7bf5eca6e
Dong Hu <cdhu@uwaterloo.ca>
2017-05-24 20:12:29 +0000

    Added homepage for testing

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,21 @@
 
 const express = require('express');
 const router  = express.Router();
+// const async   = require('async');
+
+// Takes in localStorage and creates an array with item_id and item_quantity
+// const createOrder = (cart) => {
+//   const order = [];
+//   cart.products.forEach((product) => {
+//     order.push(
+//       {
+//         item.id: product.item_id,
+//         quantity: product.quantity
+//       }
+//     );
+//   })
+//   return order;
+// }
 
 module.exports = (knex) => {
 
--- server.js
+++ server.js
@@ -16,9 +16,6 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
-// const homeRoutes = require("./routes/homeRoutes");
-// const registerRoutes = require("./routes/registerRoutes");
-// const foodRoutes = require("./routes/foodRoutes");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
