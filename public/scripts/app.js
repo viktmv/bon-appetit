8a688b7bd0434701825b031f85a9b4e650d1c639
KJAVA19811815 <karunsingh1993@gmail.com>
2017-05-24 19:34:36 +0000

    first commit to solve conflict

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,23 @@
 $(() => {
   $.ajax({
-    method: "GET",
-    url: "/api/users"
+    method: 'GET',
+    url: '/api/users'
   }).done((users) => {
     for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+      $('<div>').text(user.name).appendTo($('body'));
     }
-  });;
+  });
+
+
+  $.ajax({
+    method: 'GET',
+    url: '/users/:id'
+  }).done((restaurants) => {
+    for(rest of restaurants) {
+      $('<div>').text(rest.name).appendTo($('body'))
+    }
+  })
+
+  $.ajax({
+  })
 });
--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,108 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
+module.exports = (knex) => {
+  function checkUser(email, password) {
+  for(let user in users) {
+    if(users[user].email === email  && bcrypt.compareSync(password, users[user].password)){
+      return user;
+    }
+  }
+  return "";
+}
+
+  router.get('/', (req, res) => {
     knex
-      .select("*")
-      .from("users")
+      .select('*')
+      .from('users')
       .then((results) => {
         res.json(results);
     });
   });
 
+  router.post('/', (req, res) => {
+  let email = req.body.email;
+  let pass = req.body.password;
+  //res.cookie('user_id', userId)
+  if (!req.body.email || !req.body.password) {
+    res.sendStatus(400);  // Bad Request
+  } else {
+      let userId = checkUser(email, pass);
+      if(userId) {
+        req.session.user_id = userId;
+        res.redirect("/users/:id")
+      } else {
+          res.sendStatus(400);
+        }
+    }
+  });
+
+
+  router.get('/users/:id', (req, res) => {
+    knex
+      .select('*')
+      .from('restaurants')
+      .then((results) => {
+        res.json(results);
+      });
+  });
+
+  router.get('/menu/:id', (req, res) => {
+    knex
+      .select('*')
+      .from('restaurant_menu')
+      .then((results) => {
+        res.join(results);
+      });
+
+  });
+
+  router.get('/users/:id/active', (req, res) => {
+    knex
+      .select('*')
+      .from('orders')
+      .then((results) => {
+        res.join(results);
+
+      });
+  })
+
+  router.get('/restaurants', (req, res) => {
+
+  });
+
+  router.get('/restaurants/:id/active', (req, res) => {
+    knex
+      .select('*')
+      .from('orders')
+      .then((results) => {
+        res.join(results);
+      });
+  })
+
+
   return router;
+
 }
+
+
+
+
+
+
+
+
+// middleware that is specific to this router
+//router.use(function timeLog (req, res, next) {
+  //console.log('Time: ', Date.now())
+  //next()
+//})
+// define the home page route
+//router.get('/', function (req, res) {
+  //res.send('Birds home page')
+//})
+// define the about route
+//router.get('/about', function (req, res) {
+  //res.send('About birds')
+//})
