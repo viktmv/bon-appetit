3db773f494fe78bf560e78aafc29ddbfb91ae129
viktmv <viktmv@icloud.com>
2017-05-24 21:40:39 +0000

    fixed app js ajax req

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,18 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {
+  }).done(users => {
     for (user of users) {
       $("<div>").text(user.name).appendTo($("body"));
+    }
+  })
+
+  $.ajax({
+    method: "GET",
     url: "/restaurants"
-  }).done((restaurants) => {
+  }).done(restaurants => {
     for(restaurant of restaurants) {
       $("<div>").text(restaurant.name).appendTo($("body"));
     }
-  });
+  })
+})
