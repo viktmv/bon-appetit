66748109537928cfec5e362a64ec15916919822a
viktmv <viktmv@icloud.com>
2017-05-25 18:43:46 +0000

    implement restaurant login

---
+++ public/scripts/authentication.js
@@ -0,0 +1,40 @@
+$(() => {
+  $('#loginForm').on('submit', function(e) {
+    e.preventDefault()
+
+    let username = $('#username').val()
+    let password = $('#pwd').val()
+
+    $.ajax('/login', {
+      method: 'POST',
+      data: { username, password }
+    })
+    .done((data) => { if (data.length > 0) window.location.reload() })
+    .fail(err => console.log(err))
+  })
+  $('#logout-btn').on('click', function(e) {
+    e.preventDefault()
+
+    $.ajax('/logout', {
+      method: 'POST',
+    }).done(() => {
+      console.log('session cleared')
+      window.location.reload()
+    })
+  })
+
+  $('.restaurant-auth').on('submit', function(e) {
+    e.preventDefault()
+
+    let restaname = $('#restaname').val()
+    let password = $('#pwd').val()
+
+    console.log(restaname, password)
+    $.ajax('/restaurants/login', {
+      method: 'POST',
+      data: { restaname, password }
+    })
+    .done((data) => { if (data.length > 0) window.location.reload() })
+    .fail(err => console.log(err))
+  })
+})
--- routes/restaroutes.js
+++ routes/restaroutes.js
@@ -8,12 +8,7 @@ module.exports = (knex) => {
   // For now just renders json
   // Should render restaurant login file
   router.get('/', (req, res) => {
-    knex
-      .select('*')
-      .from('restaurant')
-      .then((results) => {
-        res.json(results);
-      });
+    res.status(200).render('restaurants')
   });
 
   // Render active orders for logged-in restaurant owner
@@ -34,7 +29,7 @@ module.exports = (knex) => {
   // Login request
   // TODO: Connect to DB for password checking
   router.post('/login', (req, res) => {
-    let restaname = req.body.username;
+    let restaname = req.body.restaname;
     let password = req.body.password;
 
     if (!restaname) return res.sendStatus(403);
