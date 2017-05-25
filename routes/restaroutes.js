1f51ae91a807babb4f8aefc0103341a1b69ec093
viktmv <viktmv@icloud.com>
2017-05-25 17:30:23 +0000

    add restaurant login & logout

--- routes/restaroutes.js
+++ routes/restaroutes.js
@@ -25,7 +25,7 @@ module.exports = (knex) => {
         res.json(results);
       });
   });
-  
+
   // Handle request for order completion
   router.post('/:id/complete', (req, res) => {
     res.send('Order completed, restaurant ' + req.params.id);
@@ -34,14 +34,33 @@ module.exports = (knex) => {
   // Login request
   // TODO: Connect to DB for password checking
   router.post('/login', (req, res) => {
-    if (!req.body.rest_id) return res.sendStatus(403)
-    req.session.rest_id = req.body.rest_id
-    res.sendStatus(202)
+    let restaname = req.body.username;
+    let password = req.body.password;
+
+    if (!restaname) return res.sendStatus(403);
+    if (!password) return res.sendStatus(403);
+
+    knex('restaurant')
+    .where({
+      restaname,
+      password
+    })
+    .select()
+    .then((restaData) => {
+      if (restaData.length === 0) return res.sendStatus(404)
+      req.session.restaname = restaData[0].restaname
+      res.status(200).send(restaData)
+    })
+    .catch(err => {
+      console.log('something happend', err)
+      res.sendStatus(500)
+    })
   });
 
   // Logout request
   router.post('/logout', (req, res) => {
-    res.sendStatus(202)
+    req.session = null;
+    res.sendStatus(200)
   });
 
   return router;
