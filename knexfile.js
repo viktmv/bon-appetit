eb48da5f75b9ef6db5cb8cce35bf5f3df540aca8
Dong Hu <cdhu@uwaterloo.ca>
2017-07-04 18:34:15 -0400

    Heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -2,18 +2,17 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-
 module.exports = {
 
   development: {
     client: 'postgresql',
     connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: process.env.DB_SSL
     },
     migrations: {
       directory: './db/migrations',
@@ -32,8 +31,11 @@ module.exports = {
       max: 10
     },
     migrations: {
+      directory: './db/migrations',
       tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
-
 };
--- server.js
+++ server.js
@@ -115,11 +115,10 @@ app.post('/logout', (req, res) => {
 
 // Postgres Heroku
 pg.defaults.ssl = true;
-console.log('process',process.env.DATABASE_URL);
+// console.log('process',process.env.DATABASE_URL);
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-
   client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
