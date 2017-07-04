af25f4ad51a5e2286575076c5e3aa61b03efbe55
Dong Hu <cdhu@uwaterloo.ca>
2017-07-04 16:47:17 -0400

    Fix heroku

--- knexfile.js
+++ knexfile.js
@@ -31,8 +31,11 @@ module.exports = {
       max: 10
     },
     migrations: {
+      directory: './db/migrations',
       tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
-
 };
