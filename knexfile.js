31564d82f4d4afb6ba21efee256e5556753bdfee
Dong Hu <cdhu@uwaterloo.ca>
2017-07-04 17:39:48 -0400

    Deploy

--- knexfile.js
+++ knexfile.js
@@ -25,13 +25,17 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: postgres://sgqcnsontnagdd:02d9c85e5e0064f774c3a4a5762a865d8f1ea0f28641b1e75cbedb079332dad7@ec2-23-23-86-179.compute-1.amazonaws.com:5432/dep6qavmgaa590,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'migrations'
+      directory: './db/migrations'
+      // tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
 };
